b3a88ffce7970553771aa65037b4a4190b33f4c7
Morrigan <morrigan.dev@gmail.com>
2021-09-14 22:24:56 +0200

    LanguageManager weiterentwickelt

--- java-utils-resources/src/main/java/de/morrigan/dev/utils/resources/LanguageManager.java
+++ java-utils-resources/src/main/java/de/morrigan/dev/utils/resources/LanguageManager.java
@@ -1,5 +1,15 @@
 package de.morrigan.dev.utils.resources;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +23,94 @@ public class LanguageManager {
   }
 
   private static final LanguageManager INSTANCE = new LanguageManager();
+
+  private enum Bundle {
+    LABELS, MESSAGES, ERRORS
+  }
+
+  private Map<Locale, ResourceBundle> labels;
+  private Map<Locale, ResourceBundle> messages;
+  private Map<Locale, ResourceBundle> errors;
+
+  private LanguageManager() {
+    super();
+    this.labels = new HashMap<>();
+    this.messages = new HashMap<>();
+    this.errors = new HashMap<>();
+  }
+
+  public String getLabel(String key, Locale locale) {
+    return getValue(Bundle.LABELS, key, locale);
+  }
+
+  public String getLabel(String key) {
+    return getValue(Bundle.LABELS, key, Locale.getDefault());
+  }
+
+  public void loadLabelsFromResources(String baseName, Locale locale) {
+    loadLabelsFromResources(baseName, Optional.of(locale));
+  }
+
+  public void loadLabelsFromResources(String baseName) {
+    loadLabelsFromResources(baseName, Optional.empty());
+  }
+
+  public Set<String> getLabelKeys() {
+    return getLabelKeys(Locale.getDefault());
+  }
+
+  public Set<String> getLabelKeys(Locale locale) {
+    Set<String> result = new HashSet<>();
+    ResourceBundle labelsBundle = getResourceBundle(Bundle.LABELS, locale);
+    Enumeration<String> keys = labelsBundle.getKeys();
+    while (keys.hasMoreElements()) {
+      result.add(keys.nextElement());
+    }
+    return result;
+  }
+
+  private String getValue(Bundle bundleName, String key, Locale locale) {
+    String result = "";
+    try {
+      ResourceBundle resourceBundle = getResourceBundle(bundleName, locale);
+      result = resourceBundle.getString(key);
+    } catch (MissingResourceException e) {
+      LOG.warn("No value found for the key '{}' in the resource bundle '{}'. Details: {}", key, bundleName.name(),
+          e.getMessage());
+    }
+    return result;
+  }
+
+  private ResourceBundle getResourceBundle(Bundle bundle, Locale locale) {
+    ResourceBundle resourceBundle;
+    switch (bundle) {
+      case LABELS:
+        resourceBundle = this.labels.get(locale);
+      break;
+      case MESSAGES:
+        resourceBundle = this.messages.get(locale);
+      break;
+      case ERRORS:
+        resourceBundle = this.errors.get(locale);
+      break;
+
+      default:
+        throw new IllegalStateException("Missing mapping for bundle: " + bundle);
+    }
+    if (resourceBundle == null) {
+      throw new MissingResourceException("No resource bundle available for the language " + locale.getDisplayLanguage()
+          + ". Please use a load method for your language before receiving a value.", bundle.name(), "");
+    }
+    return resourceBundle;
+  }
+
+  private void loadLabelsFromResources(String baseName, Optional<Locale> optLocale) {
+    if (optLocale.isPresent()) {
+      Locale locale = optLocale.get();
+      this.labels.put(locale, ResourceBundle.getBundle(baseName, locale));
+    } else {
+      this.labels.put(Locale.getDefault(), ResourceBundle.getBundle(baseName));
+    }
+  }
 
 }
---
+++ java-utils-resources/src/test/java/de/morrigan/dev/test/utils/resources/LanguageManagerTest.java
@@ -0,0 +1,84 @@
+package de.morrigan.dev.test.utils.resources;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import de.morrigan.dev.utils.resources.LanguageManager;
+
+public class LanguageManagerTest {
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
+
+  private LanguageManager sut = LanguageManager.getInstance();
+
+  @Before
+  public void setup() {
+    ResourceBundle.clearCache();
+  }
+
+  @Test
+  public void testLoadLabelsFromResource() {
+    this.sut.loadLabelsFromResources("language/lang");
+    assertThat(this.sut.getLabelKeys(), hasSize(1));
+    assertThat(this.sut.getLabel("helloWorld"), is(equalTo("Hello World!")));
+  }
+
+  @Test
+  public void testLoadLabelsFromResourceWithLocalGerman() {
+    this.sut.loadLabelsFromResources("language/labels", Locale.GERMANY);
+    assertThat(this.sut.getLabelKeys(), hasSize(1));
+    assertThat(this.sut.getLabel("helloWorld"), is(equalTo("Hallo Welt!")));
+  }
+
+  @Test
+  public void testLoadLabelsFromResourceWithMissingBaseName() {
+    MissingResourceException exception = assertThrows(MissingResourceException.class, () -> {
+      this.sut.loadLabelsFromResources("language/missing", Locale.GERMANY);
+    });
+    assertThat(exception.getMessage(), containsString("language/missing"));
+  }
+
+  @Test
+  public void testGetLabelWithExistingKey() {
+    this.sut.loadLabelsFromResources("language/labels");
+    assertThat(this.sut.getLabel("helloWorld"), is(equalTo("Hallo Welt!")));
+  }
+
+  @Test
+  public void testGetLabelWithExistingKeyAndLocaleFR() {
+    this.sut.loadLabelsFromResources("language/labels");
+    this.sut.loadLabelsFromResources("language/labels", Locale.FRANCE);
+    assertThat(this.sut.getLabel("helloWorld", Locale.FRANCE), is(equalTo("Bonjour le monde!")));
+  }
+
+  @Test
+  public void testGetLabelWithMissedLoadingLocaleFR() {
+    this.sut.loadLabelsFromResources("language/labels");
+    assertThat(this.sut.getLabel("helloWorld", Locale.FRANCE), is(equalTo("")));
+    // TODO Logger warning prüfen.
+    // by morrigan on 14.09.2021
+  }
+
+  @Test
+  public void testGetLabelWithNotExistingKey() {
+    this.sut.loadLabelsFromResources("language/labels");
+    assertThat(this.sut.getLabel("hello"), is(equalTo("")));
+    // TODO Logger warning prüfen.
+    // by morrigan on 14.09.2021
+  }
+
+}
