24dc928c0305e5682232de431e48b6a701e18bdd
Thomas Gattinger <thomas.gattinger@fsa.valantic.com>
2021-10-07 22:14:33 +0200

    getOptConfig and getConfig added with different behaviour

--- java-utils-resources/src/main/java/de/morrigan/dev/utils/resources/ConfigManager.java
+++ java-utils-resources/src/main/java/de/morrigan/dev/utils/resources/ConfigManager.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.helpers.MessageFormatter;
 
 /**
  * Hier werden alle Konfigurationen verwaltet, die für eine Anwendung benötigt werden.
@@ -14,41 +15,53 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class ConfigManager {
 
-  /**
-   * @return einzige Instanz dieses Managers.
-   */
-  public static ConfigManager getInstance() {
-    return INSTANCE;
-  }
+   /**
+    * @return einzige Instanz dieses Managers.
+    */
+   public static ConfigManager getInstance() {
+      return INSTANCE;
+   }
 
-  private static final ConfigManager INSTANCE = new ConfigManager();
+   private static final ConfigManager INSTANCE = new ConfigManager();
 
-  private Properties configs;
+   private Properties configs;
+   private String configFilename;
 
-  private ConfigManager() {
-    super();
+   private ConfigManager() {
+      super();
 
-    this.configs = new Properties();
-  }
+      this.configs = new Properties();
+   }
 
-  public Optional<String> getConfig(String configKey) {
-    return getConfigInternal(configKey);
-  }
+   public Optional<String> getOptConfig(String configKey) {
+      return getConfigInternal(configKey);
+   }
 
-  public void loadAllConfigsFromResources(String filename) throws IOException {
-    this.configs.load(getClass().getResourceAsStream(StringUtils.join("/", filename)));
-  }
+   public String getConfig(String configKey) {
+      Optional<String> optConfigValue = getConfigInternal(configKey);
+      if (optConfigValue.isPresent()) {
+         return optConfigValue.get();
+      } else {
+         throw new IllegalArgumentException(MessageFormatter.arrayFormat("Requested configuration with the key {} is not present in the configuration file {}",
+                  new Object[] { configKey, this.configFilename }).getMessage());
+      }
+   }
 
-  public Set<String> getConfigKeys() {
-    return this.configs.stringPropertyNames();
-  }
+   public void loadAllConfigsFromResources(String filename) throws IOException {
+      this.configFilename = filename;
+      this.configs.load(getClass().getResourceAsStream(StringUtils.join("/", filename)));
+   }
 
-  public void clear() {
-    this.configs.clear();
-  }
+   public Set<String> getConfigKeys() {
+      return this.configs.stringPropertyNames();
+   }
 
-  private Optional<String> getConfigInternal(String configKey) {
-    String value = this.configs.getProperty(configKey);
-    return value == null ? Optional.empty() : Optional.of(value);
-  }
+   public void clear() {
+      this.configs.clear();
+   }
+
+   private Optional<String> getConfigInternal(String configKey) {
+      String value = this.configs.getProperty(configKey);
+      return value == null ? Optional.empty() : Optional.of(value);
+   }
 }
--- java-utils-resources/src/test/java/de/morrigan/dev/test/utils/resources/ConfigManagerTest.java
+++ java-utils-resources/src/test/java/de/morrigan/dev/test/utils/resources/ConfigManagerTest.java
@@ -3,9 +3,11 @@ package de.morrigan.dev.test.utils.resources;
 import static com.spotify.hamcrest.optional.OptionalMatchers.emptyOptional;
 import static com.spotify.hamcrest.optional.OptionalMatchers.optionalWithValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -18,44 +20,59 @@ import de.morrigan.dev.utils.resources.ConfigManager;
 
 public class ConfigManagerTest {
 
-  private ConfigManager sut = ConfigManager.getInstance();
+   private ConfigManager sut = ConfigManager.getInstance();
 
-  @Before
-  public void setup() {
-    this.sut.clear();
-  }
+   @Before
+   public void setup() {
+      this.sut.clear();
+   }
 
-  @After
-  public void tearDown() {
-    this.sut.clear();
-  }
+   @After
+   public void tearDown() {
+      this.sut.clear();
+   }
 
-  @Test
-  public void testLoadConfigs() throws IOException {
-    this.sut.loadAllConfigsFromResources("config.properties");
-    assertThat(this.sut.getConfigKeys(), hasSize(1));
-  }
+   @Test
+   public void testLoadConfigs() throws IOException {
+      this.sut.loadAllConfigsFromResources("config.properties");
+      assertThat(this.sut.getConfigKeys(), hasSize(1));
+   }
 
-  @Test
-  public void testGetConfigWithMissingKey() {
-    Optional<String> config = this.sut.getConfig("missing");
-    assertThat(config, is(emptyOptional()));
-  }
+   @Test
+   public void testGetOptConfigWithMissingKey() {
+      Optional<String> config = this.sut.getOptConfig("missing");
+      assertThat(config, is(emptyOptional()));
+   }
 
-  @Test
-  public void testGetConfigWithExistingKey() throws IOException {
-    this.sut.loadAllConfigsFromResources("config.properties");
-    Optional<String> config = this.sut.getConfig("serverMode");
-    assertThat(config, is(optionalWithValue()));
-    assertThat(config.get(), is(equalTo("local")));
-  }
+   @Test
+   public void testGetOptConfigWithExistingKey() throws IOException {
+      this.sut.loadAllConfigsFromResources("config.properties");
+      Optional<String> config = this.sut.getOptConfig("serverMode");
+      assertThat(config, is(optionalWithValue()));
+      assertThat(config.get(), is(equalTo("local")));
+   }
 
-  @Test
-  public void testClear() throws IOException {
-    this.sut.loadAllConfigsFromResources("config.properties");
-    assertThat(this.sut.getConfigKeys(), hasSize(1));
-    this.sut.clear();
-    assertThat(this.sut.getConfigKeys(), hasSize(0));
-  }
+   @Test
+   public void testGetConfigWithMissingKey() {
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> this.sut.getConfig("missing"));
+      assertThat(exception.getMessage(), containsString("missing"));
+      assertThat(exception.getMessage(), containsString("is not present"));
+      assertThat(exception.getMessage(), containsString("config.properties"));
+   }
+
+   @Test
+   public void testGetConfigWithExistingKey() throws IOException {
+      this.sut.loadAllConfigsFromResources("config.properties");
+      String config = this.sut.getConfig("serverMode");
+      assertThat(config, is(equalTo("local")));
+   }
+
+   @Test
+   public void testClear() throws IOException {
+      this.sut.loadAllConfigsFromResources("config.properties");
+      assertThat(this.sut.getConfigKeys(), hasSize(1));
+      this.sut.clear();
+      assertThat(this.sut.getConfigKeys(), hasSize(0));
+   }
 
 }
